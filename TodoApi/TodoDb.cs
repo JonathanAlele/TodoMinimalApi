2cb86c33803ab33e92204bdd73b7030eb9f3a4e7
JonathanAlele <jonathanalele@gmail.com>
2024-04-19 19:17:50 +0200

    Created the DB context class

---
+++ TodoApi/TodoDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi
+{
+	// Central point of contact between the Model and the DB
+	public class TodoDb : DbContext
+	{
+
+		public DbSet<Todo> Todos => Set<Todo>();
+		public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+		{
+
+		}
+	}
+}
