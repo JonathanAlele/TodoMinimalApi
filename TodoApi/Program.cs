0528a3a33a9ada782db8d4ebd08e218810a82407
JonathanAlele <jonathanalele@gmail.com>
2024-04-20 00:08:23 +0200

    Refactored DB methods for easier unit testing by returning TypedResults

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -11,44 +11,14 @@ var app = builder.Build();
 
 var todoItems = app.MapGroup("/todoitems");
 // endpoints
-todoItems.MapGet("/", async (TodoDb db) => await db.Todos.ToListAsync());
-todoItems.MapGet("/complete", async (TodoDb db) => await db.Todos.Where(t => t.IsComplete).ToListAsync());
-todoItems.MapGet("/{id}", async (int id, TodoDb db) => await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound());
+todoItems.MapGet("/", GetAllTodos);
+todoItems.MapGet("/complete", GetCompleteTodos);
+todoItems.MapGet("/{id}", GetTodo);
 
-todoItems.MapPost("/", async (Todo todo, TodoDb db) =>
-{
-	db.Todos.Add(todo);
-	await db.SaveChangesAsync();
+todoItems.MapPost("/", CreateTodo);
 
-	return Results.Created($"/todoitems/{todo.Id}", todo);
-});
-
-todoItems.MapPut("/{id}", async (int id, Todo inputTodo, TodoDb db) =>
-{
-	var todo = await db.Todos.FindAsync(id);
-
-	if (todo is null)
-	{
-		return Results.NotFound();
-	}
-
-	todo.Name = inputTodo.Name;
-	todo.IsComplete = inputTodo.IsComplete;
-
-	await db.SaveChangesAsync();
-	return Results.NoContent();
-});
-todoItems.MapDelete("/{id}", async (int id, TodoDb db) =>
-{
-	if (await db.Todos.FindAsync(id) is Todo todo)
-	{
-		db.Todos.Remove(todo);
-		await db.SaveChangesAsync();
-		return Results.NoContent();
-	}
-
-	return Results.NotFound();
-});
+todoItems.MapPut("/{id}", UpdateTodo);
+todoItems.MapDelete("/{id}", DeleteTodo);
 //
 
 app.Run();
