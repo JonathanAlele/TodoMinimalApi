f02cba0eb0a3dc0947056d15165ca2411680c6c6
JonathanAlele <jonathanalele@gmail.com>
2024-04-19 19:14:46 +0200

    Created a class that models the data for the CRUD operations

---
+++ TodoApi/Todo.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApi
+{
+
+	// Data model for the app
+	public class Todo
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+
+		public bool IsComplete { get; set; }
+	}
+}
